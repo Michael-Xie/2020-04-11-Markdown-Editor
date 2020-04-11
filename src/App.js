7debb1ff262dcb687393b3d6e383c317d4b78c1c
Michael-Xie <xiemicha@gmail.com>
2020-04-11 13:50:06 -0400

    Initial commit with editor working; no forms of highlight at the moment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Editor from "./components/Editor";
+
+function App() {
+  return (
+    <Editor />
+  );
+}
+
+export default App;
